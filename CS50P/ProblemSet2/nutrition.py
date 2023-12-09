bcd95bed3d144494f6b766c746bf41688fdcf630
Jossany Moura <mjossany@gmail.com>
2023-12-09 20:12:45 -0300

    Finish nutrition problem

---
+++ CS50P/ProblemSet2/nutrition.py
@@ -0,0 +1,35 @@
+def main():
+    userInput = input("Item: ")
+    getFruitCalories(userInput)
+
+def getFruitCalories(userInput):
+
+    fruitsCaloriesDict = {
+        "apple": "130",
+        "avocado": "50",
+        "banana": "110",
+        "cantaloupe": "50",
+        "grapefruit": "60",
+        "grapes": "90",
+        "honeydew Melon": "50",
+        "kiwifruit": "90",
+        "lemon": "15",
+        "lime": "20",
+        "nectarine": "60",
+        "orange": "80",
+        "peach": "60",
+        "pear": "100",
+        "pineapple": "50",
+        "plums": "70",
+        "strawberries": "50",
+        "sweet cherries": "100",
+        "tangerine": "50",
+        "watermelon": "80"
+    }
+
+    if userInput.lower() in fruitsCaloriesDict:
+        print(f"Calories: {fruitsCaloriesDict[userInput.lower()]}")
+    else:
+        print("", end="")
+
+main()
